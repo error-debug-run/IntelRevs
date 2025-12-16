c0615266f20b2fb3870858834e936d8ed292257f
Rishi <rishimurmu1005@gmail.com>
2025-12-16 17:07:33 +0530

    Implement scraper manager and parser dispatch

--- go-scraper/internal/manager/manager.go
+++ go-scraper/internal/manager/manager.go
@@ -1,6 +1,8 @@
 package manager
 
 import (
+	"time"
+
 	"github.com/error-debug-run/go-scraper/internal/parser"
 	"github.com/error-debug-run/go-scraper/internal/worker"
 )
@@ -11,19 +13,31 @@ type ScrapeResult struct {
 }
 
 func RunScrapeJob(url string) (*ScrapeResult, error) {
+	site := detector.DetectSite(url)
 
-	rawHTML, err := worker.FetchPage(url)
+	var p parser.Parser
+	switch site {
+	case detector.SiteReddit:
+		p = parser.NewRedditParser()
+	default:
+		p = parser.NewGenericParser()
+	}
+
+	worker := worker.NewHTTPWorker()
+	html, err := worker.FetchHTML(url)
 	if err != nil {
 		return nil, err
 	}
 
-	reviews, err := parser.ExtractReviews(rawHTML)
+	reviews, err := p.Parse(html)
 	if err != nil {
 		return nil, err
 	}
 
 	return &ScrapeResult{
-		URL:     url,
-		Reviews: reviews,
+		URL:       url,
+		Reviews:   reviews,
+		Source:    site.String(),
+		Timestamp: time.Now().Unix(),
 	}, nil
 }
